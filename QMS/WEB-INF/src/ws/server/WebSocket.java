97209298601f4883566446954ca1bcaa3b6a62a1
japiking <37947897+japiking@users.noreply.github.com>
2018-05-15 14:00:24 +0900

    websocket java

--- QMS/WEB-INF/src/ws/server/WebSocket.java
+++ QMS/WEB-INF/src/ws/server/WebSocket.java
@@ -5,7 +5,7 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.server.ServerEndpoint;
 
-
+@ServerEndpoint("/websocket")
 public class WebSocket {
     /***
      * 웹 소켓이 연결되면 호출되는 이벤트
@@ -13,5 +13,32 @@ public class WebSocket {
     @OnOpen
     public void handleOpen(){
         System.out.println("client is now connected...");
+    }
+    /**
+     * 웹 소켓으로부터 메시지가 오면 호출되는 이벤트
+     * @param message
+     * @return
+     */
+    @OnMessage
+    public String handleMessage(String message){
+        System.out.println("receive from client : "+message);
+        String replymessage = "echo "+message;
+        System.out.println("send to client : "+replymessage);
+        return replymessage;
+    }
+    /**
+     * 웹 소켓이 닫히면 호출되는 이벤트
+     */
+    @OnClose
+    public void handleClose(){
+        System.out.println("client is now disconnected...");
+    }
+    /**
+     * 웹 소켓이 에러가 나면 호출되는 이벤트
+     * @param t
+     */
+    @OnError
+    public void handleError(Throwable t){
+        t.printStackTrace();
     }
 }
